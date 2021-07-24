c9a90a19122fc6b8fa2ecd741d03cc2d3bf5305e
Iain Shorter <iainshorter@gmail.com>
2021-07-24 13:21:42 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,90 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2020': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 11,
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'no-unused-vars': [
-			'error',
-			{ 'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_' },
-		]
-	}
+  'env': {
+    'browser': true,
+    'es2020': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 11,
+    'sourceType': 'module',
+    'tsconfigRootDir': __dirname,
+    'project': ['./tsconfig.json'],
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // match const global primitives and ensure they use UPPER_CASE
+      {
+        selector: ['variable'],
+        modifiers: ['const', 'global'],
+        types: ['boolean', 'string', 'number', 'array'],
+        format: ['UPPER_CASE'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      // match const global complex values and ensure they use PascalCase or snake_case
+      {
+        selector: ['variable'],
+        modifiers: ['const', 'global'],
+        format: ['UPPER_CASE', 'snake_case'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      // allow any case for destructured variables, to allow greater compatibility
+      // with other libraries which may use other formats
+      {
+        selector: ['variable'],
+        modifiers: ['destructured'],
+        format: null,
+      },
+      // all other variables should be snake_case
+      {
+        selector: ['variableLike'],
+        format: ['snake_case'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      // ensure static methods and members are PascalCase
+      {
+        selector: ['memberLike'],
+        modifiers: ['static'],
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      // ensure types are PascalCase
+      {
+        selector: ['typeLike'],
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': [
+      'error',
+      { 'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_' },
+    ],
+    'curly': 'error',
+  }
 };
